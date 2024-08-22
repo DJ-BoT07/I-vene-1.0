973492c7a13d788941958652e1fcdc9428a318e7
DJ-BoT07 <djmangaonkar7@gmail.com>
2024-08-22 19:22:57 +0530

    Analytics added

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/react"; // Example import, adjust as needed
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,12 +14,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={inter.className}>
-        <Toaster/>
-        {children}
+      <html lang="en">
+        <body className={inter.className}>
+          <Toaster />
+          {children}
+          <Analytics debug={false}/> {/* Add Analytics here */}
         </body>
-    </html>
+      </html>
     </ClerkProvider>
   );
 }
