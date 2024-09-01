8f1377a301216ea2d6158c63873ab4e772d67a30
DJ-BoT07 <djmangaonkar7@gmail.com>
2024-09-01 13:29:54 +0530

    Nothing

--- app/dashboard/_components/loading2.js
+++ app/dashboard/_components/loading2.js
@@ -10,6 +10,7 @@ export default function Loading2() {
 				<div className="h-7 bg-slate-400 w-full rounded-md"></div>
 				<div className="h-7 bg-slate-400 w-full rounded-md"></div>
 				<div className="h-7 bg-slate-400 w-full rounded-md"></div>
+				
 				<div className="h-7 bg-slate-400 w-1/2 rounded-md"></div>
 			</div>
 		</div>
